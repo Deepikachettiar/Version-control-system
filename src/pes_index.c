cbd4355ee6d99ad298b9cd57f959368905e9f306
deepikachettiar <deepika28226@gmail.com>
2026-04-20 07:21:03 -0700

    Phase 2: Integrated object_write to hash and store added files

--- src/pes_index.c
+++ src/pes_index.c
@@ -3,26 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Phase 2 - Commit 1: Read the file the user wants to add
 int index_add(const char *filename) {
     FILE *f = fopen(filename, "rb");
-    if (!f) {
-        printf("Error: Could not open file %s\n", filename);
-        return -1;
-    }
+    if (!f) return -1;
     
-    // Find out how big the file is
     fseek(f, 0, SEEK_END);
     size_t size = ftell(f);
     fseek(f, 0, SEEK_SET);
     
-    // Read the file into memory
     void *buf = malloc(size);
-    if (buf) {
-        fread(buf, 1, size, f);
-    }
+    fread(buf, 1, size, f);
     fclose(f);
     
-    free(buf); // Clean up for now
+    // Phase 2 - Commit 2: Hash the file and save to the Object Store
+    char hash[65];
+    if (object_write(buf, size, hash) != 0) {
+        printf("Error: Failed to write object to vault.\n");
+        free(buf);
+        return -1;
+    }
+    
+    free(buf);
     return 0;
 }
