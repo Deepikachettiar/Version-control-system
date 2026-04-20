6e9e84f6b64cceaae255781280bb7b816455854e
deepikachettiar <deepika28226@gmail.com>
2026-04-20 07:21:42 -0700

    Phase 2: Setup file pointer to append to .pes/index

--- src/pes_index.c
+++ src/pes_index.c
@@ -6,23 +6,27 @@
 int index_add(const char *filename) {
     FILE *f = fopen(filename, "rb");
     if (!f) return -1;
-    
     fseek(f, 0, SEEK_END);
     size_t size = ftell(f);
     fseek(f, 0, SEEK_SET);
-    
     void *buf = malloc(size);
     fread(buf, 1, size, f);
     fclose(f);
     
-    // Phase 2 - Commit 2: Hash the file and save to the Object Store
     char hash[65];
     if (object_write(buf, size, hash) != 0) {
-        printf("Error: Failed to write object to vault.\n");
         free(buf);
         return -1;
     }
-    
     free(buf);
+    
+    // Phase 2 - Commit 3: Open the .pes/index file to record the entry
+    FILE *index_file = fopen(".pes/index", "a"); // "a" stands for append mode
+    if (!index_file) {
+        printf("Error: Could not open index file.\n");
+        return -1;
+    }
+    
+    fclose(index_file); // We will write to it in the next commit
     return 0;
 }
